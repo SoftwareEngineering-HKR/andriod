d2316f4e541432ff96f9963933c2ee58940bce79
Tibor Blascsok <tiborblascsok02@gmail.com>
2026-02-20 13:20:03 +0100

    DeviceCardViewModel and localization

--- app/src/main/java/se/hkr/andriod/ui/screens/devicecard/DeviceCardViewModel.kt
+++ app/src/main/java/se/hkr/andriod/ui/screens/devicecard/DeviceCardViewModel.kt
@@ -4,6 +4,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Devices
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 data class DeviceScreenUiState(
     val deviceName: String = "Device Name",
@@ -16,5 +19,36 @@ data class DeviceScreenUiState(
 )
 
 class DeviceCardViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(DeviceScreenUiState())
+    val uiState: StateFlow<DeviceScreenUiState> = _uiState
 
+    fun toggleDevice(enabled: Boolean) {
+        _uiState.value = _uiState.value.copy(isEnabled = enabled)
+    }
+
+    fun toggleSchedule() {
+        // Will be implemented later
+    }
+
+    fun setDeviceData(
+        deviceName: String,
+        roomName: String,
+        isOnline: Boolean,
+        isEnabled: Boolean,
+        icon: ImageVector,
+        scheduleExpanded: Boolean,
+        lastUpdatedText: String
+    ) {
+        _uiState.update {
+            it.copy(
+                deviceName = deviceName,
+                roomName = roomName,
+                isOnline = isOnline,
+                isEnabled = isEnabled,
+                icon = icon,
+                scheduleExpanded = scheduleExpanded,
+                lastUpdatedText = lastUpdatedText
+            )
+        }
+    }
 }

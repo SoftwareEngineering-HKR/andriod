31162e00d48c941fce0d973ff42f592e50ef1d5a
Tibor Blascsok <tiborblascsok02@gmail.com>
2026-03-11 16:12:38 +0100

    add AddDeviceViewModel

--- app/src/main/java/se/hkr/andriod/ui/screens/adddevice/AddDeviceViewModel.kt
+++ app/src/main/java/se/hkr/andriod/ui/screens/adddevice/AddDeviceViewModel.kt
@@ -1,6 +1,9 @@
 package se.hkr.andriod.ui.screens.adddevice
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import se.hkr.andriod.navigation.DeviceType
 
 data class AddDeviceUiState(
@@ -24,5 +27,55 @@ enum class ConnectionType {
 }
 
 class AddDeviceViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(AddDeviceUiState())
+    val uiState: StateFlow<AddDeviceUiState> = _uiState
 
+    fun onDeviceNameChanged(deviceName: String) {
+        _uiState.update {
+            it.copy(deviceName = deviceName)
+        }
+    }
+
+    fun onRoomSelected(room: String) {
+        _uiState.update {
+            it.copy(selectedRoom = room)
+        }
+    }
+
+    fun addRoom(room: String) {
+        _uiState.update {
+            it.copy(rooms = it.rooms + room,
+                selectedRoom = room)
+        }
+    }
+
+    fun onDeviceTypeSelected(deviceType: DeviceType) {
+        _uiState.update {
+            it.copy(selectedDeviceType = deviceType)
+        }
+    }
+
+    fun onConnectionTypeSelected(connectionType: ConnectionType) {
+        _uiState.update {
+            it.copy(connectionType = connectionType)
+        }
+    }
+
+    fun onAddDeviceClick() {
+        val state = _uiState.value
+
+        if (
+            state.deviceName.isBlank() ||
+            state.selectedRoom == null ||
+            state.selectedDeviceType == null ||
+            state.connectionType == null
+        ) {
+            _uiState.update {
+                it.copy(error = "Please fill in all fields")
+            }
+            return
+        }
+
+        // TODO: Save Device and Room + set up connection with device
+    }
 }

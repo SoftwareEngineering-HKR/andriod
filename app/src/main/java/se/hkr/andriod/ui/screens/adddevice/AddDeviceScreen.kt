4890c4fdc0ef168750f7f2e78492ee6c1a9cad9a
Tibor Blascsok <tiborblascsok02@gmail.com>
2026-03-11 16:29:52 +0100

    AddDevice top bar

--- app/src/main/java/se/hkr/andriod/ui/screens/adddevice/AddDeviceScreen.kt
+++ app/src/main/java/se/hkr/andriod/ui/screens/adddevice/AddDeviceScreen.kt
@@ -1,2 +1,71 @@
 package se.hkr.andriod.ui.screens.adddevice
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import se.hkr.andriod.R
+import se.hkr.andriod.ui.theme.lightBlue
+
+@Composable
+fun AddDeviceScreen(
+    viewModel: AddDeviceViewModel,
+    onBackClick: () -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.lightBlue)
+            .padding(top = 24.dp)
+    ) {
+        // TODO: Make a component for top bar
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(0.9f)
+                    .padding(bottom = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = onBackClick) {
+                    Icon(Icons.AutoMirrored.Rounded.ArrowBack, null)
+                }
+
+                Text(
+                    text = stringResource(R.string.add_device),
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.weight(1f),
+                    textAlign = TextAlign.Center
+                )
+
+                Spacer(modifier = Modifier.size(48.dp))
+            }
+        }
+    }
+
+}

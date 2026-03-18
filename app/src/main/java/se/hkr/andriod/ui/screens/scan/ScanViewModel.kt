7562d4f19bbc54a986d9ebf170b27d08dec43411
Tibor Blascsok <tiborblascsok02@gmail.com>
2026-03-18 16:56:39 +0100

    ScanViewModel + UiState
    - Added fake retrying

--- app/src/main/java/se/hkr/andriod/ui/screens/scan/ScanViewModel.kt
+++ app/src/main/java/se/hkr/andriod/ui/screens/scan/ScanViewModel.kt
@@ -1,2 +1,36 @@
 package se.hkr.andriod.ui.screens.scan
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+data class ScanUiState(
+    val isRetrying: Boolean = false
+)
+
+class ScanViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(ScanUiState())
+    val uiState: StateFlow<ScanUiState> = _uiState
+
+    fun onRetryClick() {
+        if (!_uiState.value.isRetrying) return
+
+        viewModelScope.launch {
+            _uiState.update {
+                it.copy(isRetrying = true)
+            }
+
+            // Fake scan
+            delay(1500)
+
+            _uiState.update {
+                it.copy(isRetrying = false)
+            }
+        }
+    }
+
+}

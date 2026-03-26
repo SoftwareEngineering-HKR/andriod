54b34c6cf061a7061b27e77d1222dc530863869f
Tibor Blascsok <tiborblascsok02@gmail.com>
2026-03-26 13:59:01 +0100

    Language Viewmodel
    Currently without saving the changes

--- app/src/main/java/se/hkr/andriod/ui/screens/settings/subscreens/language/LanguageViewModel.kt
+++ app/src/main/java/se/hkr/andriod/ui/screens/settings/subscreens/language/LanguageViewModel.kt
@@ -1,5 +1,9 @@
 package se.hkr.andriod.ui.screens.settings.subscreens.language
 
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import se.hkr.andriod.data.language.AppLanguage
 
 data class LanguageUiState(
@@ -13,4 +17,30 @@ data class LanguageUiState(
 ) {
     val hasChanges: Boolean
         get() = selectedLanguage != appliedLanguage
+}
+
+class LanguageViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(LanguageUiState())
+    val uiState: StateFlow<LanguageUiState> = _uiState
+
+    fun onLanguageSelected(language: AppLanguage) {
+        _uiState.update {
+            it.copy(selectedLanguage = language)
+        }
+    }
+
+    fun onSaveClicked() {
+        val state = _uiState.value
+        if (!state.hasChanges || state.isSaving) {
+            return
+        }
+
+        _uiState.update {
+            it.copy(isSaving = true)
+        }
+
+        _uiState.update {
+            it.copy(isSaving = false, appliedLanguage = it.selectedLanguage)
+        }
+    }
 }

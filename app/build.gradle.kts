24327f17ae52d4e59b543f0e5acdeb8246e19d9c
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2026-04-17 02:09:53 +0200

    AppNavGraph now does a check to see if authsession is logged in. If it is logged in it will skip the login screen and navigate to the overview page.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.datastore.core)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.compose.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

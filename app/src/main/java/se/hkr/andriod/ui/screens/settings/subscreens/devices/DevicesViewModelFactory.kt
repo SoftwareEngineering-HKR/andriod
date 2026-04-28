963b2b3ccec8d4460946d6753058aa1bef0b8d17
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2026-04-28 14:33:20 +0200

    DevicesViewModel now fetches and loads all devices and rooms instead of relying on mock data or initial devices. Refactored DeviceViewModelFactory and MainScreen to send along an instance of roomStore to DevicesViewModel.

--- app/src/main/java/se/hkr/andriod/ui/screens/settings/subscreens/devices/DevicesViewModelFactory.kt
+++ app/src/main/java/se/hkr/andriod/ui/screens/settings/subscreens/devices/DevicesViewModelFactory.kt
@@ -3,15 +3,17 @@ package se.hkr.andriod.ui.screens.settings.subscreens.devices
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import se.hkr.andriod.data.network.DeviceStore
+import se.hkr.andriod.data.network.RoomStore
 
 class DevicesViewModelFactory(
-    private val deviceStore: DeviceStore
+    private val deviceStore: DeviceStore,
+    private val roomStore: RoomStore
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DevicesViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return DevicesViewModel(deviceStore) as T
+            return DevicesViewModel(deviceStore, roomStore) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
